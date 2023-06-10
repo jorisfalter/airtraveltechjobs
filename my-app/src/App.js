11bb0ede9d4ca5808f2ce74f6612be0bcb27c417
Joris Falter <joris@jorisfalter.com>
2023-06-10 20:30:52 +0700

    x

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Featured from "./Featured";
 
 function App() {
   return (
@@ -27,46 +28,7 @@ function App() {
               </div>
             </div>
             <div className="row">
-              <div className="col-md-4">
-                <div className="card mb-4 shadow-sm">
-                  <svg
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c" />
-                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                      Thumbnail
-                    </text>
-                  </svg>
-                  <div className="card-body">
-                    <p className="card-text">This is a cool company</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          Edit
-                        </button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Featured />
               <div className="col-md-4">
                 <div className="card mb-4 shadow-sm">
                   <svg
