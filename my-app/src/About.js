b06aee92c1f3364f8c26b790baa19d1b966724bf
Joris Falter <joris@jorisfalter.com>
2023-08-25 20:25:35 +0700

    x

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -27,49 +27,55 @@ function About() {
             </p>
           </div>
         </section>
-        <p>
-          Welcome to Air Travel Tech Jobs, a job board for everyone interested
-          in Air Travel and Technology.
-        </p>
-
-        <p></p>
-
-        <p>I'm vetting these companies.</p>
-        <p>Based on which companies I think I would like.</p>
-        <p>Obviously, this is highly subjective!</p>
-        <p>
-          But I don't want another dull job board, I want opinions, character,
-          discussion, ...
-        </p>
-        <p>
-          {" "}
-          I want people to help people find interesting roles at fun companies!
-        </p>
-
-        <p>If I make a mistake, I'll add it here.</p>
-
-        <p>
-          This is a slow project. I'll provide regular updates to the website,
-          nevertheless cool air travel companies don't just pop up out of the
-          ground every week.{" "}
-        </p>
-        <p>
-          Best case scenario: with new developments around drones transporting
-          people and electric planes, we might be on the threshold of a new
-          flying revolution. Fingers crossed.
-        </p>
-
-        <h1>Community</h1>
-        <p>Feel free to join the Slack group. </p>
-
-        <h1>Blog</h1>
-        <p>The rise of the German drone taxi industry. Coming Soon.</p>
-        <p>Where is the innovation happening in Air Travel. Coming Soon.</p>
-        <h1>Fun</h1>
-
-        {/* <!-- fun --> */}
         <div className="album py-5 bg-light ">
           <div className="container homepage-wrapper">
+            <p>Hi, I'm Joris &#128075;</p>
+            <p>Welcome to Air Travel Tech Jobs.</p>
+
+            <p>
+              I started this site because I was building Air Travel related apps
+              (see <i>fun</i> below) and figured it would be cool to work in the
+              air travel industry.
+            </p>
+
+            <p></p>
+
+            <p>I'm vetting these companies.</p>
+            <p>Based on which companies I think I would like.</p>
+            <p>Obviously, this is highly subjective!</p>
+            <p>
+              But I don't want another dull job board, I want opinions,
+              character, discussion, ...
+            </p>
+            <p>
+              {" "}
+              I want people to help people find interesting roles at fun
+              companies!
+            </p>
+
+            <p>If I make a mistake, I'll add it here.</p>
+
+            <p>
+              This is a slow project. I'll provide regular updates to the
+              website, nevertheless cool air travel companies don't just pop up
+              out of the ground every week.{" "}
+            </p>
+            <p>
+              Best case scenario: with new developments around drones
+              transporting people and electric planes, we might be on the
+              threshold of a new flying revolution. Fingers crossed.
+            </p>
+
+            <h1>Community</h1>
+            <p>Feel free to join the Slack group. </p>
+
+            <h1>Blog</h1>
+            <p>The rise of the German drone taxi industry. Coming Soon.</p>
+            <p>Where is the innovation happening in Air Travel. Coming Soon.</p>
+            <h1>Fun</h1>
+
+            {/* <!-- fun --> */}
+
             <div className="row">
               <div className="col-md-5">
                 <a href={"https://randomairport.onrender.com/"} target="_blank">
@@ -142,12 +148,12 @@ function About() {
                 .{" "}
               </div>
             </div>
+            <i>
+              Inspired by <a href="www.keyvalues.com">Key Values</a>, RemoteOK,
+              and many other job boards.
+            </i>
           </div>
         </div>
-        <i>
-          Inspired by <a href="www.keyvalues.com">Key Values</a>, RemoteOK, and
-          many other job boards.
-        </i>
       </main>
       <Footer />
     </div>
